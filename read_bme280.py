186c39ecd5362878ee5c1c97ca43a6e1e6a0ca37
Brian C. Ray <bcr666@gmail.com>
2025-07-10 13:23:22 -0500

    Merging with changes already on main branch

--- read_bme280.py
+++ read_bme280.py
@@ -11,12 +11,13 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x77)
 
 #Loop to print data
 while True:
-	temperatureC = bme280.temperature
-	temperatureF = temperatureC*9/5+32
-	print("Temperature: %.2f C" % temperatureC)
-	print("Temperature: %.2f F" % temperatureF)
-	print("Humidity: %.2f %%" % bme280.humidity)
-	print("Pressure: %.2f hpa" % bme280.pressure)
-	print("-----------------------------")
-	time.sleep(5)
+  print(bme280)
+  temperatureC = bme280.temperature
+  temperatureF = temperatureC*9/5+32
+  print("Temperature: %.2f C" % temperatureC)
+  print("Temperature: %.2f F" % temperatureF)
+  print("Humidity: %.2f %%" % bme280.humidity)
+  print("Pressure: %.2f hpa" % bme280.pressure)
+  print("-----------------------------")
+  time.sleep(5)
 
